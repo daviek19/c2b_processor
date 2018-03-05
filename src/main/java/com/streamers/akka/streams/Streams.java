696378f0cc445eaf50922f099e707f2bd06a3f5c
David Mwangi <daviek19@gmail.com>
2018-03-05 16:25:08 +0300

    fixed the parsing engine from StringByte to line by line

--- src/main/java/com/streamers/akka/streams/Streams.java
+++ src/main/java/com/streamers/akka/streams/Streams.java
@@ -4,9 +4,13 @@ import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
+import akka.stream.javadsl.Framing;
+import akka.stream.javadsl.FramingTruncation;
 import akka.stream.javadsl.Source;
 import akka.util.ByteString;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import scala.concurrent.duration.FiniteDuration;
 
 public class Streams {
@@ -14,6 +18,10 @@ public class Streams {
     public static void main(String[] argv) throws InterruptedException, Exception {
         ActorSystem _system = ActorSystem.create("c2b_processor");
         final Materializer materializer = ActorMaterializer.create(_system);
+        final List<String> strings = new ArrayList<>();
+
+        final String newLine = "\n";
+        final ByteString newLineBytes = ByteString.fromString(newLine);
 
         Source<ByteString, NotUsed> source = FileTailSource
                 .create(
@@ -21,12 +29,12 @@ public class Streams {
                         1,
                         0,
                         FiniteDuration.fromNanos(1000 * 5)
-                );
+                )
+                .via(Framing.delimiter(newLineBytes, 100, FramingTruncation.ALLOW));
+
         source.runForeach(
                 x -> {
-                    char c = x.utf8String().charAt(0);
-                    
-                    System.out.println(x + " END");
+                    System.out.println( x.utf8String());
                 }, materializer
         );
     }
